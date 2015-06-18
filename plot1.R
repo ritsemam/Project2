b3f9c3538ff20d6f800609e0cfcf0c8c40ec8031
Matt Ritsema <ritsemam@gmail.com>
2015-06-17 23:59:14 -0400

    Exploratory Data

--- plot1.R
+++ plot1.R
@@ -3,13 +3,15 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-#Aggregate data by year
+#Aggregate data by emissions
 
 emissions <- aggregate(NEI[c("Emissions")], list(year = NEI$year), sum)
 
 #plot using base plot
 
 png('plot1.png', width=480, height=480)
-plot(emissions$year, emission$Emissions, type = "l", 
+plot(emissions$year, emissions$Emissions, type = "l", 
      main = "Total Emissions from PM2.5 in the US",
      xlab = "Year", ylab = "Emissions")
+
+dev.off()
--- plot2.R
+++ plot2.R
@@ -3,16 +3,16 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-#Aggregate data by year
-balt <- subset(NEI, fibs == "24051")
-emissions <- aggregate(balt[c("Emissions")], list(year = NEI$year), sum)
+#Aggregate data by emissions
+balt <- subset(NEI, fips == "24510")
+emissions <- aggregate(balt[c("Emissions")], list(year = balt$year), sum)
 
 
 
 #plot using base plot
 
-png('plot1.png', width=480, height=480)
-plot(emissions$year, emission$Emissions, type = "l", 
+png('plot2.png', width=480, height=480)
+plot(emissions$year, emissions$Emissions, type = "l", 
      main = "Total Emissions from PM2.5 in Baltimore City",
      xlab = "Year", ylab = "Emissions")
 
